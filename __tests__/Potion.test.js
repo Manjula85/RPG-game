da661fe923fdd13d415af562d2f11817d2408389
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-02-10 16:52:41 -0500

    section 2 done

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,7 +1,11 @@
 const Potion = require('../lib/Potion.js');
-jest.mock('../lib/Potion');
 
-//console.log(new Potion());
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
+
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
 
 test('creates a random potion object', () => {
   const potion = new Potion();
